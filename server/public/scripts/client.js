b1e70c5c665f5bba7846a985a1937cd6d7c8f728
Christian Fredricks <Christianpfred@gmail.com>
2021-07-28 16:29:42 -0500

    I have a understanding of Postman and Ajax.

---
+++ server/public/scripts/client.js
@@ -0,0 +1,34 @@
+$(document).ready(onReady);
+
+function onReady() {
+    console.log('So ready!');
+
+    getQuotes()
+}
+
+function getQuotes() {
+    $.ajax({
+        method: 'GET',
+        url: '/quotes',
+    })
+        //Network request take a LONG time
+        //wait for the request to complete and THEN, run this function
+        //Whatever we pass to res.send()
+        //becomes the 'response argument
+        .then(function(response) {
+            console.log('GET /quotes responce', response)
+
+          let quoteList =  $('#quotes');
+          console.log('quotes list element', quoteList)
+
+          for (let quote of response) {
+              console.log('quote is', quote)
+              quoteList.append(`
+                <li>
+                    ${quote.text}
+                    <em> -- by ${quote.author}</em>
+                </li>
+              `);
+          }
+        }
+)};
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const { text } = require('express');
 //Load the express libary
 //from node_modules/express
 const express = require('express');
+// loaf the body parser module
+const bodyParser = require('body-Parser');
 
 // create our "app" (Server)
 const app = express();
@@ -28,6 +30,12 @@ const quotes = [
 // aka "Static assets"
 app.use(express.static('./server/public'));
 
+//Setup body parser
+//Tells express how to read data from the client
+// either as JSON, or url-encoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
 // Listen for requests coming to a specfic URL: http://localhost:5000/quotes
 app.get('/quotes', function(req, res) {
     console.log('Ready to send back some quotes');
@@ -35,7 +43,31 @@ app.get('/quotes', function(req, res) {
 
     // Send back data to the client
     // array of quotes objects
-    res.send([quotes]);
+    res.send(quotes);
+});
+
+app.post('/quotes', function(req,res){
+    console.log('woo hoom we got a new quote');
+
+    // Body parser gives us req.body
+    console.log('req.body', req.body);
+    let newQuote = req.body;
+
+if (!newQuote.author || !newQuote.text) {
+    // Set status code to 400 (client messed up)
+    // and send back a useful message in the response body
+    res.status(400).send({
+        message: "Missing a reported field! Try again, and try harder."
+    });
+    // End the function!
+    // Otherwise, we'll end up being bad
+    return;
+}
+    quotes.push(newQuote);
+
+    // Send back a status code (no body)
+    res.sendStatus(201); // 201 Created
+
 });
 
 //Listen for Requests
